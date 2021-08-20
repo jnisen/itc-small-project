b79a6e90f4098d383ff20eb9dfe8246cd8259c4c
Jony Nisen <jnisen@gmail.com>
2021-08-20 10:07:22 +0300

    add path public

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.use(express.static('public'));
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'))
 app.listen(port, function () { return console.log('App Listening on port', port); });
