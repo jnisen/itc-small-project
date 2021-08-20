41247b53432566357eff65a16168c10fcfde129f
Jony Nisen <jnisen@gmail.com>
2021-08-20 10:04:47 +0300

    add public + index + h2

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    res.send('<h1>Good App</h1>');
-});
+app.use(express.static('public'));
 app.listen(port, function () { return console.log('App Listening on port', port); });
--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.get('/', (req, res) => {
-    res.send('<h1>Good App</h1>')
-})
+app.use(express.static('public'))
 
 app.listen(port, ()=> console.log('App Listening on port', port))
